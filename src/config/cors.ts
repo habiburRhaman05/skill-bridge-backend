f76ea71f706e38e4e74e7e1429448cece544d958
habiburRhaman05 <devhabib2005@gmail.com>
2026-01-29 17:23:06 +0600

    update cors setting and add frontend url

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,5 +1,5 @@
 export const corsConfig = {
-  origin: ['http://localhost:3000','https://nuance-daily.vercel.app','https://example.com',"https://postella-beta.vercel.app"],
+  origin: ['http://localhost:3000',"https://skill-bridge-frontend-gamma.vercel.app"],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
   credentials: true,
