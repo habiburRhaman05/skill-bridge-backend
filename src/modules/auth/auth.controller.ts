b5a32a533006b876263714a7efeb3d8191059306
Habibur Rhaman <devhabib2005@gmail.com>
2026-02-02 10:19:50 +0600

    Update cookie expire or maxage time

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -25,7 +25,7 @@ import { sendSuccess } from "../../utils/apiResponse";
   try {
     const { user, token } = await authServices.loginUser(req.body);
      res.cookie('token', token, { 
-       maxAge: 24 * 60 * 60 * 1000,
+       maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly:true,
         secure:true,
         sameSite:"none",
